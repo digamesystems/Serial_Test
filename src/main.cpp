2c7fe0c0d68dc70379b9f92d2408d974355ac2c3
John Price <john.price@digamesystems.com>
2024-08-01 16:32:25 -0700

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -32,24 +32,34 @@ void loop() {
   int messageLength = message.length() + 1 ; // +1 for null terminator
   Serial.println("message length: " + String(messageLength));
 
+  // SEND
   int t0 = micros(); // start time
 
   mySerial2.print(message);
   mySerial2.flush();  // ******* Warning: This causes the program to wait unitl the TX buffer is empty.
                       // If you don't flush, the program will continue and the message will be sent 
-                      // in the background. Making your send time look faster! ******
+                      // in the background. Making your send time look faster! We're doing this to be
+                      // sure the data has been sent for timing measurements. ******
+                      // 
+                      // In real life, you might want not not flush but move on in the program flow and 
+                      // and do other things while the device you are talking do does things and starts
+                      // sending back data.  
 
   int t1 = micros(); // end time
 
   Serial.println("Time to send: " + String(t1 - t0) + " microseconds.");
   Serial.println("Time per character: " + String((t1 - t0) / messageLength) + " microseconds.");
 
+  // RECEIVE
   t0 = micros(); // start time
-  while (mySerial2.available()) {
+
+  while (mySerial2.available()) {  // Data should be available since we just sent it through the loopback 
+                                   // and the TX buffer got emptied. 
+                                   // In real life you might want to spin until you get a response.
     char inChar = (char)mySerial2.read();
-    //Serial.print(inChar);
+    // In real life, we'd do something with the incoming data here.
   }
-  //Serial.println();
+
   t1 = micros(); // end time
 
   Serial.println("Time to read: " + String(t1 - t0) + " microseconds.");
